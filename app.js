ae239f68077ef0ed7118cdd6b9ce566ee46ec427
Zach <zach000317@gmail.com>
2025-07-24 15:17:29 +0800

    feat: 去除一些无用代码

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var categoryRouter = require("./routes/category");
 var tagRouter = require("./routes/tag");
@@ -15,10 +14,6 @@ var app = express();
 const authentication = require("./config/authentication");
 const camelCaseResponse = require("./config/camelCaseResponse");
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,7 +23,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(authentication);
 app.use(camelCaseResponse);
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/category", categoryRouter);
 app.use("/tag", tagRouter);
