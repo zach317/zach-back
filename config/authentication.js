8c0c6d06ebc14f0228bc26c4703d8f0ab085afa4
Zach <zach000317@gmail.com>
2025-08-19 10:47:58 +0800

    fix: 🐞token验证逻辑bug修复&&优化

--- config/authentication.js
+++ config/authentication.js
@@ -1,6 +1,11 @@
 const JWT = require("../utils/jwt");
 
-const excludeUrl = ["/users/login", "/users/register", "/users/check-username"];
+const excludeUrl = [
+  "/users/login",
+  "/users/register",
+  "/users/check-username",
+  "user/find-user",
+];
 const authentication = (req, res, next) => {
   if (excludeUrl.includes(req.url)) {
     next();
@@ -11,8 +16,18 @@ const authentication = (req, res, next) => {
     const payload = JWT.verify(token);
     if (payload) {
       // 重新计算token过期时间
-      const { username, id } = payload;
-      const newToken = JWT.generate({ username, id }, "1h");
+      const { username, id, exp } = payload;
+      const now = Math.floor(Date.now() / 1000);
+      let newToken;
+      if (exp - now < 600) {
+        // 如果少于 10 分钟过期
+        newToken = JWT.generate(
+          { username: payload.username, id: payload.id },
+          "1h"
+        );
+      } else {
+        newToken = JWT.generate({ username, id }, "1h");
+      }
       res.header("Authorization", newToken);
       req.body = { ...req.body, id };
       next();
@@ -22,7 +37,8 @@ const authentication = (req, res, next) => {
     res.status(401).send({ success: false, message: "登录超时，请重新登陆" });
     return;
   }
-  next();
+  // 没有token也直接返回401
+  res.status(401).send({ success: false, message: "登录超时，请重新登陆" });
 };
 
 module.exports = authentication;
